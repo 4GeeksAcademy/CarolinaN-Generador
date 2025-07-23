c3ff63df16d5483e2f1a477f4959b60cdac609dd
CarolinaNFeria <rogua20.12@gmail.com>
2025-07-23 18:01:08 +0000

    fin

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ let button = document.getElementById("generateBtn");
 let list = document.getElementById("domainList");
 
 button.addEventListener("click", () => {
-  let pronouns = ["my", "your", "our", "the"];
-  let adjectives = ["cool", "fast", "big", "funny"];
-  let nouns = ["dog", "code", "idea", "world"];
-  let extensions = [".com", ".net", ".io", ".org"];
+  let pronouns = ["my", "your", "our"];
+  let adjectives = ["cool", "fast", "big"];
+  let nouns = ["dog", "code", "world"];
+  let extensions = [".com", ".net", ".org"];
 
   list.innerHTML = ""; // Clear previous list
 
