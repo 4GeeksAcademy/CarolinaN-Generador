66bb4aa6411b9977a1fd21d2fcc8686d1ea8258d
CarolinaNFeria <rogua20.12@gmail.com>
2025-07-23 17:59:15 +0000

    por fin

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import "bootstrap";
-import "./style.css";
 
+let button = document.getElementById("generateBtn");
+let list = document.getElementById("domainList");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+button.addEventListener("click", () => {
+  let pronouns = ["my", "your", "our", "the"];
+  let adjectives = ["cool", "fast", "big", "funny"];
+  let nouns = ["dog", "code", "idea", "world"];
+  let extensions = [".com", ".net", ".io", ".org"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  list.innerHTML = ""; // Clear previous list
+
+  for (let pronoun of pronouns) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let ext of extensions) {
+          let domain = `${pronoun}${adjective}${noun}${ext}`;
+          let li = document.createElement("li");
+          li.textContent = domain;
+          list.appendChild(li);
+        }
+      }
+    }
+  }
+});
